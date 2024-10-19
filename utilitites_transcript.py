c7b6591e886ab7b60b563d3ae5228ab65da5e020
mbringas <56489741+mbringas@users.noreply.github.com>
2024-10-18 21:21:16 -0300

    Update utilitites_transcript.py

--- utilitites_transcript.py
+++ utilitites_transcript.py
@@ -7,6 +7,10 @@ Utilidades desgrabado
 #should be
 #     list_of_files=["Baila_Morena","Tu_Gatita"]
 list_of_files=[]
+#Define here the extension of files
+# I understand avi,mp3,mkv,mp4 files should work.
+extension="mp3"
+
 
 !pip install git+https://github.com/openai/whisper.git
 
@@ -14,7 +18,7 @@ import whisper
 model = whisper.load_model("turbo")
 
 for i in list_of_files:
-  result = model.transcribe(f"{i}.mp3")
+  result = model.transcribe(f"{i}.{extension}")
   print(result["text"])
   with open(f"transcript_{i}.txt", "a") as f:
     print(result["text"], file=f)
